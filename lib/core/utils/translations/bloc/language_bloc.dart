f87a6e7da1b0c2525a5df4986400f23699b04d38
Thuongle2112 <2051063468@e.tlu.edu.vn>
2025-06-04 11:22:30 +0700

    feat: add nav menu and translate language

--- lib/core/utils/translations/bloc/language_bloc.dart
+++ lib/core/utils/translations/bloc/language_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import '../../../l10n/locale_keys.g.dart';
 import '../../../local/hive_storage.dart';
 import '../model/language_option.dart';
 import 'language_event.dart';
@@ -12,30 +13,30 @@ class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
   List<LanguageOption> filteredLanguages = [];
   final BuildContext context;
   LanguageBloc(this._globalStorage, this.context)
-      : super(LanguageInitial(_globalStorage.languageCode)) {
+    : super(LanguageInitial(_globalStorage.languageCode)) {
     on<ChangeLanguageEvent>(_onChangeLanguage);
     on<ToggleLanguageOptionExpansion>(_onToggleLanguageOptionExpansion);
     on<SearchLanguageEvent>(_onSearchLanguage);
   }
 
- void _onChangeLanguage(ChangeLanguageEvent event, Emitter<LanguageState> emit) {
-  print("🔄 Đổi ngôn ngữ sang: ${event.locale}");
-  _globalStorage.languageCode = event.locale;
-  context.setLocale(event.locale);
-  emit(LanguageChanged(event.locale));
-}
-
+  void _onChangeLanguage(
+    ChangeLanguageEvent event,
+    Emitter<LanguageState> emit,
+  ) {
+    print("🔄 Đổi ngôn ngữ sang: ${event.locale}");
+    _globalStorage.languageCode = event.locale;
+    context.setLocale(event.locale);
+    emit(LanguageChanged(event.locale));
+  }
 
   final List<LanguageOption> _languageOptions = [
     LanguageOption(
       locale: const Locale('en'),
-      displayName: 'English',
-      flagIcon: "assets/svg/language_icon/england-svgrepo-com.svg",
+      displayName: "English",
     ),
     LanguageOption(
       locale: const Locale('vi'),
-      displayName: 'Việt Nam',
-      flagIcon: "assets/svg/language_icon/vietnam-svgrepo-com.svg",
+      displayName: LocaleKeys.vietnamese.tr(),
     ),
   ];
 
@@ -44,23 +45,28 @@ class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
   Locale getSelectedLanguage() => _globalStorage.languageCode;
 
   void _onToggleLanguageOptionExpansion(
-      ToggleLanguageOptionExpansion event, Emitter<LanguageState> emit) {
+    ToggleLanguageOptionExpansion event,
+    Emitter<LanguageState> emit,
+  ) {
     _isLanguageOptionExpanded = !_isLanguageOptionExpanded;
     emit(LanguageToggleState(!_isLanguageOptionExpanded));
   }
 
   void _onSearchLanguage(
-      SearchLanguageEvent event, Emitter<LanguageState> emit) {
+    SearchLanguageEvent event,
+    Emitter<LanguageState> emit,
+  ) {
     try {
-      filteredLanguages = event.query.isEmpty
-          ? _languageOptions
-          : _languageOptions
-              .where(
-                (option) => option.displayName
-                    .toLowerCase()
-                    .contains(event.query.toLowerCase()),
-              )
-              .toList();
+      filteredLanguages =
+          event.query.isEmpty
+              ? _languageOptions
+              : _languageOptions
+                  .where(
+                    (option) => option.displayName.toLowerCase().contains(
+                      event.query.toLowerCase(),
+                    ),
+                  )
+                  .toList();
       emit(LanguageSearchState(event.query, filteredLanguages));
     } catch (e) {
       debugPrint('Error: $e');
